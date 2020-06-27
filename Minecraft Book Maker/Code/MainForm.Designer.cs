a2a8106f7e26cdfd9c60ad8a040f8bebdb2d7a6f
Hemok98 <hemok98@yandex.ru>
2020-06-27 22:22:19 +0200

    Init project commit

---
+++ Minecraft Book Maker/Code/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Minecraft_Book_Maker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.inputTextbox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.goRightPageButton = new System.Windows.Forms.Button();
+            this.goLeftPageButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputTextbox
+            // 
+            this.inputTextbox.Location = new System.Drawing.Point(4, 5);
+            this.inputTextbox.Multiline = true;
+            this.inputTextbox.Name = "inputTextbox";
+            this.inputTextbox.Size = new System.Drawing.Size(269, 375);
+            this.inputTextbox.TabIndex = 0;
+            this.inputTextbox.TextChanged += new System.EventHandler(this.ConvertButtonClick);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.White;
+            this.outputLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Black;
+            this.outputLabel.Location = new System.Drawing.Point(377, 5);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(205, 318);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // goRightPageButton
+            // 
+            this.goRightPageButton.BackColor = System.Drawing.Color.Transparent;
+            this.goRightPageButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.goRightPageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goRightPageButton.ForeColor = System.Drawing.Color.Transparent;
+            this.goRightPageButton.Image = global::Minecraft_Book_Maker.Properties.Resources.rightButton;
+            this.goRightPageButton.Location = new System.Drawing.Point(483, 326);
+            this.goRightPageButton.Name = "goRightPageButton";
+            this.goRightPageButton.Size = new System.Drawing.Size(100, 58);
+            this.goRightPageButton.TabIndex = 4;
+            this.goRightPageButton.UseVisualStyleBackColor = false;
+            this.goRightPageButton.Click += new System.EventHandler(this.goRightPageButton_Click);
+            // 
+            // goLeftPageButton
+            // 
+            this.goLeftPageButton.BackColor = System.Drawing.Color.Transparent;
+            this.goLeftPageButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.goLeftPageButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.goLeftPageButton.ForeColor = System.Drawing.Color.Transparent;
+            this.goLeftPageButton.Image = global::Minecraft_Book_Maker.Properties.Resources.leftButton;
+            this.goLeftPageButton.Location = new System.Drawing.Point(377, 326);
+            this.goLeftPageButton.Name = "goLeftPageButton";
+            this.goLeftPageButton.Size = new System.Drawing.Size(100, 58);
+            this.goLeftPageButton.TabIndex = 3;
+            this.goLeftPageButton.UseVisualStyleBackColor = false;
+            this.goLeftPageButton.Click += new System.EventHandler(this.goLeftPageButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Minecraft_Book_Maker.Properties.Resources._5b739c22c64474_92630279153430326681213802;
+            this.pictureBox1.Location = new System.Drawing.Point(279, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 386);
+            this.Controls.Add(this.goRightPageButton);
+            this.Controls.Add(this.goLeftPageButton);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.inputTextbox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(603, 425);
+            this.MinimumSize = new System.Drawing.Size(603, 425);
+            this.Name = "MainForm";
+            this.Text = "Minecraft Text Conventer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Button goLeftPageButton;
+        private System.Windows.Forms.Button goRightPageButton;
+    }
+}
+
---
+++ Minecraft Book Maker/Code/MainForm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Book_Maker
+{
+    public partial class MainForm : Form
+    {
+
+        private string[] pages;
+        private int pagesLength = 0;
+        int selectedPage = 0;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ConvertButtonClick(object sender, EventArgs e)
+        {
+            string alphabet = "аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩЪъЫыЬьЭэЮюЯя.,0123456789";
+            //string[] mass;
+            ArrayList words = new ArrayList(); 
+            //string[] mass = new string[2000];
+            int n = -1;
+            bool flag = false;
+
+            string text = inputTextbox.Text.Replace("\r\n"," ");
+
+            for (int j = 0; j < text.Length; j++)
+            {
+                char ch = text[j];
+
+                if (alphabet.IndexOf(ch) != -1)
+                {
+
+                    if (!flag) {
+                        n++;
+                        //mass[n] = "";
+                        words.Add("");
+                    }
+                    words[n] = (string)words[n] + ch;
+                    //mass[n] += ch;
+                    flag = true;
+                }
+                else flag = false;
+            }
+            n++;
+
+
+            int i = -1;
+            flag = false;
+
+            int list = 0;
+            pages = new string[1000];
+            for (int j = 0; j < pages.Length; j++)
+            {
+                pages[j] = "";
+            }
+
+
+            int line = 1;
+            int symbol = 0;
+            //StringBuilder sb = new StringBuilder();
+
+            for (int j = 0; j < text.Length; j++)
+            {
+                char ch = text[j];
+                if (alphabet.IndexOf(ch) != -1)
+                {
+
+                    if (!flag)
+                    {
+                        i++;
+
+
+                        if (symbol + ((string)words[i]).Length > 18)
+                        {
+                            symbol = ((string)words[i]).Length;
+                            line++;
+                            //sb.Append("\r\n");
+                            
+                            if (line > 14)
+                            {
+                                line = 1;
+                                list++;
+                                //sb.Append("==============\r\n");
+                                //cout << "==============" << endl;
+                            } else
+                            {
+                                pages[list] += "\r\n";
+                            }
+
+                        }
+                        else
+                        {
+                            symbol += ((string)words[i]).Length;
+                        }
+                        pages[list] += (string)words[i];
+                        //sb.Append(mass[i]);
+                        //cout << mass[i];
+                    }
+                    //mass[n]+=ch;
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+
+                    if (symbol + 1 > 18)
+                    {
+                        symbol = 1;
+                        line++;
+                        //sb.Append("\r\n");
+                        if (line > 14)
+                        {
+                            line = 1;
+                            list++;
+                            //sb.Append("==============\r\n");
+                            //cout << "==============" << endl;
+                        } else
+                        {
+                            pages[list] += "\r\n";
+                        }
+                    }
+                    else
+                    {
+                        symbol += 1;
+                    }
+                    if (ch == ' ' && symbol == 1)
+                    {
+                        symbol = 0;
+                    }
+                    else pages[list] += ch;
+                    //sb.Append(ch);
+                    //cout << ch;
+                }
+            }
+            pagesLength = list + 1;
+            selectedPage = 0;
+            //sb.Append("==============\r\n");
+            //cout << endl << endl << list;
+
+            this.selectedPage = 0;
+            displayToOutput();
+        }
+
+        private void goLeftPageButton_Click(object sender, EventArgs e)
+        {
+            if (this.selectedPage - 1 < 0) return;
+            this.selectedPage--;
+            displayToOutput();
+        }
+
+        private void goRightPageButton_Click(object sender, EventArgs e)
+        {
+            if (this.selectedPage + 1 >= pagesLength) return;
+            this.selectedPage++;
+            displayToOutput();
+        }
+
+        private void displayToOutput()
+        {
+            outputLabel.Text = "====== " + (selectedPage + 1) + "/" + (pagesLength) + " ======\r\n" + pages[selectedPage];
+        }
+
+        private async void outputLabel_Click(object sender, EventArgs e)
+        {
+            if (pages == null) return;
+            if (pages[selectedPage] != "") Clipboard.SetText(pages[selectedPage]);
+
+            outputLabel.BackColor = Color.LightGray;
+            await Task.Run(() => {
+                System.Threading.Thread.Sleep(200);
+                outputLabel.BackColor = Color.White;
+            });
+        }
+    }
+}
